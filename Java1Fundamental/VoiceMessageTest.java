2cd6e9e1f8eb8d79db0d893beec152f0e4acfdbc
heidi1105 <heidi1105@gmail.com>
2023-04-03 15:23:53 -0700

    VoiceMessage

--- Java1Fundamental/VoiceMessageTest.java
+++ Java1Fundamental/VoiceMessageTest.java
@@ -1,6 +1,50 @@
+import java.util.ArrayList;
+
 public class VoiceMessageTest{
     public static void main(String[] args){
         VoiceMessageUtil newBot = new VoiceMessageUtil();
         System.out.println(newBot.greetMessage());
+        System.out.println(newBot.greetMessage("Pepper"));
+        System.out.println(newBot.greetMessage("Pepper", 14));
+        System.out.println(newBot.greetMessage(13));
+
+    // 5. Casting (implicit & explicit, Null VALUE)
+    // int --> double (no loss of data)
+        int age = 14;
+        double doubleAge = age;
+        System.out.println(doubleAge);
+
+        // double--> int (explicit casting: loss of data)
+        double requiredTime = 1.5;
+        int intRequiredTime = (int) requiredTime;
+        System.out.println(intRequiredTime);
+
+        // fixed array
+        int[] messagesPerDay = new int[3];
+        messagesPerDay[0] = 5;
+        messagesPerDay[1] = 2;
+        int[] messagesPerDay2 = { 5, 8, 6};
+
+        String[] messages = { "Please call back!", 
+            "Make sure you install jdk", 
+            "Do not install VS code exztension"};
+
+        newBot.printArray(messages);
+        newBot.printArray(messagesPerDay);
+
+        // Dynamic ArrayList
+        ArrayList<String> reminders = new ArrayList<>();
+        reminders.add("DO NOT INSTALL VS CODE EXTENSION");
+        reminders.add("Stay hydrated!!");
+        reminders.add("A little bit of code and test it!");
+        reminders.add("Remind Shawn to give some jokes");
+        reminders.set(3, "Use dark mode on the replit");
+        System.out.println(reminders);
+
+        newBot.printArray(reminders);
+
+
+
+
     }
 }
--- Java1Fundamental/VoiceMessageUtil.java
+++ Java1Fundamental/VoiceMessageUtil.java
@@ -1,5 +1,41 @@
+import java.util.*;
+
 public class VoiceMessageUtil{
     public String greetMessage(){
-        return "Hi!";
+        Date currentTime = new Date();
+        return "Hi! It is " + currentTime ;
     }
+
+    public String greetMessage(String name){
+        return "Hi, "+ name;
+    }
+
+    public String greetMessage(String name, int age){
+        return "Hi, "+ name + " who is " + age + " years old.";
+    }
+
+    public String greetMessage(int count){
+        return "Hi for "+ count + " times";
+    }
+
+    // 7. for-loop
+    public void printArray(String[] arr){
+        for(String element : arr){
+            System.out.println(element);
+        }
+    }
+
+    public void printArray(int[] arr){
+        for(int i=0; i< arr.length; i++){
+            arr[i]++;
+            System.out.println(arr[i]);
+        }
+    }
+
+    public void printArray(ArrayList<String> arr){
+        for(int i=0; i<arr.size(); i++){
+            System.out.println(arr.get(i));
+        }
+    }
+
 }
